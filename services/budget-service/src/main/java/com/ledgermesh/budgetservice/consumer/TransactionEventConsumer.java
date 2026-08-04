e5ac721bd44af0848d30e41dd64036d4fbeda1ec
Romeo Rabiaj <rrabiaj@gmail.com>
2026-08-04 10:38:55 +0200

    refactor: clean up consumeTransactionCreatedEvent method in TransactionEventConsumer for improved readability

--- services/budget-service/src/main/java/com/ledgermesh/budgetservice/consumer/TransactionEventConsumer.java
+++ services/budget-service/src/main/java/com/ledgermesh/budgetservice/consumer/TransactionEventConsumer.java
@@ -1,12 +1,12 @@
 package com.ledgermesh.budgetservice.consumer;
 
+import com.ledgermesh.budgetservice.dto.BudgetEvaluationRequestDTO;
 import com.ledgermesh.budgetservice.event.TransactionCreatedEvent;
 import com.ledgermesh.budgetservice.service.BudgetService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
-import com.ledgermesh.budgetservice.dto.BudgetEvaluationRequestDTO;
 
 @Component
 public class TransactionEventConsumer {
@@ -24,27 +24,17 @@ public class TransactionEventConsumer {
             topics = "transaction-events",
             groupId = "budget-service-group"
     )
-    public void consumeTransactionCreatedEvent(
-            TransactionCreatedEvent event) {
+    public void consumeTransactionCreatedEvent(TransactionCreatedEvent event) {
+        logger.info("Budget Service received transaction event: {}",event);
 
-        logger.info(
-                "Budget Service received transaction event: {}",
-                event
-        );
-
-        logger.info(
-                "Evaluating budget for userId={}, category={}, amount={}",
-                event.getUserId(),
-                event.getCategory(),
-                event.getAmount()
-        );
-
+        logger.info("Evaluating budget for userId={}, category={}, amount={}",event.getUserId(),event.getCategory(),event.getAmount());
         budgetService.evaluateTransaction(
-        BudgetEvaluationRequestDTO.builder()
-                .userId(event.getUserId())
-                .category(event.getCategory())
-                .amount(event.getAmount())
-                .build()
-	);
-    }
+                BudgetEvaluationRequestDTO.builder()
+                        .userId(event.getUserId())
+                        .category(event.getCategory())
+                        .amount(event.getAmount())
+                        .build()
+        );
+        }
+
 }
